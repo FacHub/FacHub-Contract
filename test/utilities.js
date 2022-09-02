062c1334f83ab7729be901d4c708f38097095e10
FacHub <94888182+FacHub@users.noreply.github.com>
2022-09-02 17:38:39 +0800

    Update utilities.js

--- test/utilities.js
+++ test/utilities.js
@@ -10,3 +10,4 @@ getAddress,
   solidityPack
 } from 'ethers/utils'
 const PERMIT_TYPEHASH = keccak256(
+  toUtf8Bytes('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)')
